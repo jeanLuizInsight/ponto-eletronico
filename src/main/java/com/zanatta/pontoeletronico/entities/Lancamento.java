431d3e2228ec35e47486171647b9ecc137cde9f9
Jean Luiz <jl.zanatta92@yahoo.com.br>
2019-06-24 21:11:58 -0300

    Ajustes na entidade Lancamento

--- src/main/java/com/zanatta/pontoeletronico/entities/Lancamento.java
+++ src/main/java/com/zanatta/pontoeletronico/entities/Lancamento.java
@@ -1,5 +1,133 @@
 package com.zanatta.pontoeletronico.entities;
 
-public class Lancamento {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.zanatta.pontoeletronico.enums.PerfilEnum;
+import com.zanatta.pontoeletronico.enums.TipoEnum;
+
+@Entity
+@Table(name = "lancamento")
+public class Lancamento implements Serializable {
+
+	private static final long serialVersionUID = 57312213516328940L;
+
+	private Long id;
+	private Date data;
+	private String descricao;
+	private String localizacao;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	private TipoEnum tipo;
+	private Funcionario funcionario;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "data", nullable = false)
+	public Date getData() {
+		return data;
+	}
+
+	@Column(name = "descricao", nullable = true)
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Column(name = "localizacao", nullable = true)
+	public String getLocalizacao() {
+		return localizacao;
+	}
+
+	@Column(name = "data_criacao", nullable = false)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	@Column(name = "data_atualizacao", nullable = false)
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "tipo", nullable = false)
+	public TipoEnum getTipo() {
+		return tipo;
+	}
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public void setTipo(TipoEnum tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	@PrePersist
+	public void prePersist() {
+		Date data = new Date();
+		this.dataCriacao = data;
+		this.dataAtualizacao = data;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		this.dataAtualizacao = new Date();
+	}
+
+	@Override
+	public String toString() {
+		return "Lancamento [id=" + id + ", data=" + data + ", tipo=" + tipo + ", funcionario=" + funcionario + "]";
+	}
 
 }
