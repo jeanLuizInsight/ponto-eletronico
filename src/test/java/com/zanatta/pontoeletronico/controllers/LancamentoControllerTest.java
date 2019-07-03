a6272729c80a60d036e838cefdb52025d25b054b
Jean Luiz Zanatta <jean.zanatta@unoesc.edu.br>
2019-07-03 16:58:03 -0300

    Criação de testes automatizados para regras de lançamentos.

---
+++ src/test/java/com/zanatta/pontoeletronico/controllers/LancamentoControllerTest.java
@@ -0,0 +1,112 @@
+package com.zanatta.pontoeletronico.controllers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zanatta.pontoeletronico.dtos.LancamentoDto;
+import com.zanatta.pontoeletronico.entities.Funcionario;
+import com.zanatta.pontoeletronico.entities.Lancamento;
+import com.zanatta.pontoeletronico.enums.TipoEnum;
+import com.zanatta.pontoeletronico.services.FuncionarioService;
+import com.zanatta.pontoeletronico.services.LancamentoService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("desenvolvimento")
+public class LancamentoControllerTest {
+
+	private static final String BASE_URL = "/api/lancamentos/";
+	private static final Long ID_FUNCIONARIO = Long.valueOf(1);
+	private static final Long ID_LANCAMENTO = Long.valueOf(1);
+	private static final String TIPO = TipoEnum.INICIO_TRABALHO.name();
+	private static final Date DATA = new Date();
+	private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	@Autowired private MockMvc mvc;
+	@MockBean private LancamentoService lancamentoService;
+	@MockBean private FuncionarioService funcionarioService;
+
+	@Test
+	public void testCadastrarLancamento() throws Exception {
+		Lancamento lancamento = this.obterDadosLancamento();
+		BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Funcionario()));
+		BDDMockito.given(this.lancamentoService.persistir(Mockito.any(Lancamento.class))).willReturn(lancamento);
+		mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
+				.content(this.obterJsonRequisicaoPost())
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.id").value(ID_LANCAMENTO))
+				.andExpect(jsonPath("$.data.tipo").value(TIPO))
+				.andExpect(jsonPath("$.data.data").value(this.dateFormat.format(DATA)))
+				.andExpect(jsonPath("$.data.funcionarioId").value(ID_FUNCIONARIO))
+				.andExpect(jsonPath("$.errors").isEmpty());
+	}
+
+	@Test
+	public void testCadastrarLancamentoFuncionarioIdInvalido() throws Exception {
+		BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong())).willReturn(Optional.empty());
+		mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
+				.content(this.obterJsonRequisicaoPost())
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.errors").value("Funcionário não encontrado. ID inexistente."))
+				.andExpect(jsonPath("$.data").isEmpty());
+	}
+
+	@Test
+	public void testRemoverLancamento() throws Exception {
+		BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Lancamento()));
+		mvc.perform(MockMvcRequestBuilders.delete(BASE_URL + ID_LANCAMENTO)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testRemoverLancamentoAcessoNegado() throws Exception {
+		BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Lancamento()));
+		mvc.perform(MockMvcRequestBuilders.delete(BASE_URL + ID_LANCAMENTO)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isForbidden());
+	}
+
+	private String obterJsonRequisicaoPost() throws JsonProcessingException {
+		LancamentoDto lancamentoDto = new LancamentoDto();
+		lancamentoDto.setId(null);
+		lancamentoDto.setData(this.dateFormat.format(DATA));
+		lancamentoDto.setTipo(TIPO);
+		lancamentoDto.setFuncionarioId(ID_FUNCIONARIO);
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(lancamentoDto);
+	}
+
+	private Lancamento obterDadosLancamento() {
+		Lancamento lancamento = new Lancamento();
+		lancamento.setId(ID_LANCAMENTO);
+		lancamento.setData(DATA);
+		lancamento.setTipo(TipoEnum.valueOf(TIPO));
+		lancamento.setFuncionario(new Funcionario());
+		lancamento.getFuncionario().setId(ID_FUNCIONARIO);
+		return lancamento;
+	}
+
+}
